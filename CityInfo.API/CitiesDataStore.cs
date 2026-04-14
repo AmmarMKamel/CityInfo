64ec5e7223e6f2497fc138624fc3d5214442ecbc
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-14 12:04:56 +0200

    feat: add PointOfInterestDto and city points of interest support

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -15,7 +15,22 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with that big park."
+                    Description = "The one with that big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States."
+                        },
+                        new()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan."
+                        }
+                    }
                 },
                 new()
                 {

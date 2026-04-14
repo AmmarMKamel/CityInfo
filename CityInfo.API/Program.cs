ace6c3c224da3b0eb1305e8fe92e9b1b6f139de9
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-14 12:05:55 +0200

    feat: add FilesController for file download endpoint

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
+
+//builder.Services.AddProblemDetails(options =>
+//{
+//    options.CustomizeProblemDetails = ctx =>
+//    {
+//        ctx.ProblemDetails.Extensions.Add("additionalInfo", "Additional Info Example");
+//    };
+//});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
